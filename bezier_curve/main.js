e4ab296b4c7b7b3db9cf2c1974eb337e5d8cf67b
x4204 <poparomarudy@gmail.com>
2017-05-26 19:09:54 +0300

    made possible for the curve to take negative values

--- bezier_curve/main.js
+++ bezier_curve/main.js
@@ -1,13 +1,11 @@
-// get canvas related references
 var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+var ct = canvas.getContext("2d");
+var width = canvas.width/3;
+var height = canvas.height/3;
+var division = width/10;
 var BB = canvas.getBoundingClientRect();
 var offsetX = BB.left;
 var offsetY = BB.top;
-var WIDTH = canvas.width;
-var HEIGHT = WIDTH;
-var division = WIDTH/10;
-
 // drag related variables
 var dragok = false;
 var startX;
@@ -21,56 +19,106 @@ var redX = document.getElementById("redX");
 var redY = document.getElementById("redY");
 var result = document.getElementById("result");
 
+// listen for mouse events
+canvas.onmousedown = myDown;
+canvas.onmouseup = myUp;
+canvas.onmousemove = myMove;
+
 // an array of objects that define different shapes
 var shapes=[];
 // define 2 circles
 shapes.push({// blue circle
   id: 0,
-  x: WIDTH*0.25,
-  y: HEIGHT*0.9,
+  x: width*0.25,
+  y: -height*0.1,
   r: 10,
   fill: "rgba(0, 0, 255, 0.8)",
   isDragging: false
 });
 shapes.push({// red circle
   id: 1,
-  x: WIDTH*0.25,
-  y: 0,
+  x: width*0.25,
+  y: -height,
   r: 10,
   fill: "rgba(255, 0, 0, 0.8)",
   isDragging: false
 });
 
-// listen for mouse events
-canvas.onmousedown = myDown;
-canvas.onmouseup = myUp;
-canvas.onmousemove = myMove;
-
-// call to draw the scene
-drawBG();
+translate(width, height*2);
 draw();
 
-// draw a single circle
-function circle(c) {
-  ctx.beginPath();
-  if(c.id == 0) ctx.moveTo(0, HEIGHT);
-  else ctx.moveTo(WIDTH, 0)
-  ctx.lineWidth = 2;
-  ctx.lineTo(c.x, c.y);
-  ctx.strokeStyle = 'rgba(0, 0, 0, 0.1)';
-  ctx.stroke();
-  ctx.closePath();
 
-  ctx.fillStyle = c.fill;
-  ctx.beginPath();
-  ctx.arc(c.x, c.y, c.r, 0, Math.PI*2);
-  ctx.closePath();
-  ctx.fill();
+function translate(x, y) {
+  ct.translate(x, y);
 }
 
-// clear the canvas
+// clear background
 function clear() {
-  ctx.clearRect(0, 0, WIDTH, HEIGHT);
+  translate(-width, -height*2);
+  ct.clearRect(0, 0, width*3, height*3);
+  translate(width, height*2);
+}
+
+// draw background
+function drawBG() {
+  translate(-width, -height*2);
+  for (var i=1; i<30; i++) {
+    // draw the horizontal lines
+    ct.beginPath();
+      ct.moveTo(0, i*division);
+      ct.lineWidth = 1;
+      ct.lineTo(width*3, i*division);
+      ct.strokeStyle = 'rgba(0, 0, 0, 0.1)';
+      ct.stroke();
+    ct.closePath();
+    // draw the vertical lines
+    ct.beginPath();
+      ct.moveTo(i*division, 0);
+      ct.lineWidth = 1;
+      ct.lineTo(i*division, height*3);
+      ct.strokeStyle = 'rgba(0, 0, 0, 0.1)';
+      ct.stroke();
+    ct.closePath();
+  }
+  // draw the inner square
+  ct.beginPath();
+    ct.rect(width, height, width, height)
+    ct.strokeStyle = 'rgba(0, 0, 0, 0.5)';
+    ct.stroke();
+  ct.closePath();
+  translate(width, height*2);
+}
+
+// draw circle
+function circle(c) {
+  ct.beginPath();
+  if(c.id == 0) ct.moveTo(0, 0);
+  else ct.moveTo(width, -height)
+  ct.lineWidth = 2;
+  ct.lineTo(c.x, c.y);
+  ct.strokeStyle = 'rgba(0, 0, 0, 0.2)';
+  ct.stroke();
+  ct.closePath();
+
+  ct.fillStyle = c.fill;
+  ct.beginPath();
+  ct.arc(c.x, c.y, c.r, 0, Math.PI*2);
+  ct.lineWidth = 6;
+  ct.strokeStyle = 'rgba(0, 0, 0, 0.15)';
+  ct.stroke();
+  ct.closePath();
+  ct.fill();
+}
+
+// draw the bezier curve
+function drawBezier() {
+  ct.beginPath();
+  ct.moveTo(0, 0);
+  ct.bezierCurveTo(shapes[0].x, shapes[0].y, shapes[1].x, shapes[1].y, width, -height);
+  ct.lineWidth = 4;
+  ct.strokeStyle = '#7eb282';
+  ct.stroke();
+  ct.closePath();
 }
 
 // redraw the scene
@@ -83,7 +131,7 @@ function draw() {
   // redraw each shape in the shapes[] array
   for(var i=0; i<shapes.length; i++) {
     // update each lever
-    circle(shapes[i]);
+      circle(shapes[i]);
     // update the coordinates
     updateCoords(shapes[i]);
   }
@@ -92,7 +140,7 @@ function draw() {
 // show coordinates
 function updateCoords(s) {
   var X = Math.floor(s.x/(division/10))/100;
-  var Y = Math.floor((WIDTH - s.y)/(division/10))/100;
+  var Y = -(Math.floor(s.y/(division/10))/100);
 
   if (s.id == 0) {
     blueX.innerHTML = "X: " + X;
@@ -105,58 +153,25 @@ function updateCoords(s) {
   }
 }
 
-// draw the bezier curve
-function drawBezier() {
-  ctx.beginPath();
-  ctx.moveTo(0, HEIGHT);
-  ctx.bezierCurveTo(shapes[0].x, shapes[0].y, shapes[1].x, shapes[1].y, WIDTH, 0);
-  ctx.lineWidth = 4;
-  ctx.strokeStyle = '#7eb282';
-  ctx.stroke();
-  ctx.closePath();
-}
-
-// draw idivisions
-function drawBG() {
-  for (var i=1; i<=10; i++) {
-    // draw horizontal lines
-    ctx.beginPath();
-    ctx.moveTo(0, i*division);
-    ctx.lineWidth = 1;
-    ctx.lineTo(WIDTH, i*division);
-    ctx.strokeStyle = 'rgba(0, 0, 0, 0.05)';
-    ctx.stroke();
-    ctx.closePath();
-    // draw vertical lines
-    ctx.beginPath();
-    ctx.moveTo(i*division, 0);
-    ctx.lineWidth = 1;
-    ctx.lineTo(i*division, WIDTH);
-    ctx.strokeStyle = '#eee';
-    ctx.stroke();
-    ctx.closePath();
-  }
-}
-
 // handle mousedown events
 function myDown(e){
-
   // tell the browser we're handling this mouse event
   e.preventDefault();
   e.stopPropagation();
 
   // get the current mouse position
-  var mx = parseInt(e.clientX - offsetX);
-  var my = parseInt(e.clientY - offsetY);
+  var mx = parseInt(e.clientX - offsetX - width);
+  var my = -parseInt(e.clientY - offsetY - height*2);
 
   // test each shape to see if mouse is inside
-  dragok=false;
+  dragok = false;
   for (var i=0; i<shapes.length; i++){
     var s = shapes[i];
     var dx = s.x - mx;
-    var dy = s.y - my;
+    var dy = -(-s.y - my);
+
     // test if the mouse is inside this circle
-    if ( dx * dx + dy * dy < s.r * s.r){
+    if (dx * dx + dy * dy < s.r * s.r){
       dragok = true;
       s.isDragging = true;
     }
@@ -179,24 +194,22 @@ function myUp(e){
   }
 }
 
-// handle mouse moves
+// handle mousemove events
 function myMove(e){
   // if we're dragging anything...
   if (dragok){
-
     // tell the browser we're handling this mouse event
     e.preventDefault();
     e.stopPropagation();
 
     // get the current mouse position
-    var mx = parseInt(e.clientX - offsetX);
-    var my = parseInt(e.clientY - offsetY);
+    var mx = parseInt(e.clientX - offsetX - width);
+    var my = -parseInt(e.clientY - offsetY - height*2);
 
     // calculate the distance the mouse has moved
     // since the last mousemove
-    var dx = mx-startX;
-    var dy = my-startY;
-
+    var dx = mx - startX;
+    var dy = -(my - startY);
     // move each rect that isDragging
     // by the distance the mouse has moved
     // since the last mousemove
@@ -214,6 +227,5 @@ function myMove(e){
     // reset the starting mouse position for the next mousemove
     startX = mx;
     startY = my;
-
   }
 }
