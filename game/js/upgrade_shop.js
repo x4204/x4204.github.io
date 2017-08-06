b054117f03121637f97b2bf9b5ede3aa30be06d0
x4204 <poparomarudy@gmail.com>
2017-08-06 14:52:52 +0300

    fix: optimized 'upgrade_shop.js'. now it takes nearly half as less space

--- game/js/upgrade_shop.js
+++ game/js/upgrade_shop.js
@@ -1,31 +1,42 @@
-// health ----------------------------------------------------------------------
+// global ----------------------------------------------------------------------
 let upgradeShopPoints = document.querySelector(`#c_upgrPts`);
+// functions -------------------------------------------------------------------
+let increaseStat = function(stat, shopCurrStat, isInversed, fixed, scale) {
+    if (Game[stat].default.toFixed(fixed) >= Game[stat].max)
+      alert(`You have reached max ${stat} limit`);
+    else if (Game.upgrades == 0)
+      alert('You have no upgrade points left');
+    else {
+      Game[stat].default += Game[stat].factor;
+      shopCurrStat.innerHTML = (Game[stat].default / scale).toFixed(fixed);
+      if (!isInversed) Game.upgrades--;
+      else Game.upgrades++;
+      upgradeShopPoints.innerHTML = Game.upgrades;
+    }
+}
+
+let decreaseStat = function(stat, shopCurrStat, isInversed, fixed, scale) {
+    if (Game[stat].default.toFixed(fixed) <= Game[stat].min)
+      alert(`You have reached min ${stat} limit`);
+    else {
+      Game[stat].default -= Game[stat].factor;
+      shopCurrStat.innerHTML = (Game[stat].default / scale).toFixed(fixed);
+      if (!isInversed) Game.upgrades++;
+      else Game.upgrades--;
+      upgradeShopPoints.innerHTML = Game.upgrades;
+    }
+}
+// health ----------------------------------------------------------------------
 let btnIncreaseHealth = document.querySelector('#btnIncreaseHealth');
 let btnDecreaseHealth = document.querySelector('#btnDecreaseHealth');
 let shopCurrHealth = document.querySelector('#c_health');
 
 btnIncreaseHealth.addEventListener('mousedown', function() {
-  if (Game.health.default >= Game.health.max)
-    alert('You have reached max health limit');
-  else if (Game.upgrades == 0)
-    alert('You have no upgrade points left');
-  else {
-    Game.health.default += Game.health.factor;
-    shopCurrHealth.innerHTML = Game.health.default / 100;
-    Game.upgrades--;
-    upgradeShopPoints.innerHTML = Game.upgrades;
-  }
+  increaseStat('health', shopCurrHealth, false, 0, 100);
 });
 
 btnDecreaseHealth.addEventListener('mousedown', function() {
-  if (Game.health.default <= Game.health.min)
-    alert('You have reached min health limit');
-  else {
-    Game.health.default -= Game.health.factor;
-    shopCurrHealth.innerHTML = Game.health.default / 100;
-    Game.upgrades++;
-    upgradeShopPoints.innerHTML = Game.upgrades;
-  }
+  decreaseStat('health', shopCurrHealth, false, 0, 100);
 });
 
 // armor -----------------------------------------------------------------------
@@ -34,27 +45,11 @@ let btnDecreaseArmor = document.querySelector('#btnDecreaseArmor');
 let shopCurrArmor = document.querySelector('#c_armor');
 
 btnIncreaseArmor.addEventListener('mousedown', function() {
-  if (Game.armor.default >= Game.armor.max)
-    alert('You have reached max armor limit');
-  else if (Game.upgrades == 0)
-    alert('You have no upgrade points left');
-  else {
-    Game.armor.default += Game.armor.factor;
-    shopCurrArmor.innerHTML = Game.armor.default / 100;
-    Game.upgrades--;
-    upgradeShopPoints.innerHTML = Game.upgrades;
-  }
+  increaseStat('armor', shopCurrArmor, false, 0, 100);
 });
 
 btnDecreaseArmor.addEventListener('mousedown', function() {
-  if (Game.armor.default <= Game.armor.min)
-    alert('You have reached min armor limit');
-  else {
-    Game.armor.default -= Game.armor.factor;
-    shopCurrArmor.innerHTML = Game.armor.default / 100;
-    Game.upgrades++;
-    upgradeShopPoints.innerHTML = Game.upgrades;
-  }
+  decreaseStat('armor', shopCurrArmor, false, 0, 100);
 });
 
 // bullet speed ----------------------------------------------------------------
@@ -63,27 +58,11 @@ let btnDecreaseBulletSpeed = document.querySelector('#btnDecreaseBulletSpeed');
 let shopCurrBulletSpeed = document.querySelector('#c_bullet_speed');
 
 btnIncreaseBulletSpeed.addEventListener('mousedown', function() {
-  if (Game['bullet speed'].default >= Game['bullet speed'].max)
-    alert('You have reached max bullet speed limit');
-  else if (Game.upgrades == 0)
-    alert('You have no upgrade points left');
-  else {
-    Game['bullet speed'].default += Game['bullet speed'].factor;
-    shopCurrBulletSpeed.innerHTML = Game['bullet speed'].default;
-    Game.upgrades--;
-    upgradeShopPoints.innerHTML = Game.upgrades;
-  }
+  increaseStat('bullet speed', shopCurrBulletSpeed, false, 1, 1);
 });
 
 btnDecreaseBulletSpeed.addEventListener('mousedown', function() {
-  if (Game['bullet speed'].default <= Game['bullet speed'].min)
-    alert('You have reached min bullet speed limit');
-  else {
-    Game['bullet speed'].default -= Game['bullet speed'].factor;
-    shopCurrBulletSpeed.innerHTML = Game['bullet speed'].default;
-    Game.upgrades++;
-    upgradeShopPoints.innerHTML = Game.upgrades;
-  }
+  decreaseStat('bullet speed', shopCurrBulletSpeed, false, 1, 1);
 });
 
 // bullet damage ---------------------------------------------------------------
@@ -92,27 +71,11 @@ let btnDecreaseBulletDamage = document.querySelector('#btnDecreaseBulletDamage')
 let shopCurrBulletDamage = document.querySelector('#c_bullet_damage');
 
 btnIncreaseBulletDamage.addEventListener('mousedown', function() {
-  if (Game['bullet damage'].default >= Game['bullet damage'].max)
-    alert('You have reached max bullet damage limit');
-  else if (Game.upgrades == 0)
-    alert('You have no upgrade points left');
-  else {
-    Game['bullet damage'].default += Game['bullet damage'].factor;
-    shopCurrBulletDamage.innerHTML = Game['bullet damage'].default;
-    Game.upgrades--;
-    upgradeShopPoints.innerHTML = Game.upgrades;
-  }
+  increaseStat('bullet damage', shopCurrBulletDamage, false, 0, 1);
 });
 
 btnDecreaseBulletDamage.addEventListener('mousedown', function() {
-  if (Game['bullet damage'].default <= Game['bullet damage'].min)
-    alert('You have reached min bullet damage limit');
-  else {
-    Game['bullet damage'].default -= Game['bullet damage'].factor;
-    shopCurrBulletDamage.innerHTML = Game['bullet damage'].default;
-    Game.upgrades++;
-    upgradeShopPoints.innerHTML = Game.upgrades;
-  }
+  decreaseStat('bullet damage', shopCurrBulletDamage, false, 0, 1);
 });
 
 // max speed -------------------------------------------------------------------
@@ -121,27 +84,11 @@ let btnDecreaseMaxSpeed = document.querySelector('#btnDecreaseMaxSpeed');
 let shopCurrMaxSpeed = document.querySelector('#c_max_speed');
 
 btnIncreaseMaxSpeed.addEventListener('mousedown', function() {
-  if (Game['max speed'].default >= Game['max speed'].max)
-    alert('You have reached max speed limit');
-  else if (Game.upgrades == 0)
-    alert('You have no upgrade points left');
-  else {
-    Game['max speed'].default += Game['max speed'].factor;
-    shopCurrMaxSpeed.innerHTML = Game['max speed'].default;
-    Game.upgrades--;
-    upgradeShopPoints.innerHTML = Game.upgrades;
-  }
+  increaseStat('max speed', shopCurrMaxSpeed, false, 2, 1);
 });
 
 btnDecreaseMaxSpeed.addEventListener('mousedown', function() {
-  if (Game['max speed'].default <= Game['max speed'].min)
-    alert('You have reached min speed limit');
-  else {
-    Game['max speed'].default -= Game['max speed'].factor;
-    shopCurrMaxSpeed.innerHTML = Game['max speed'].default;
-    Game.upgrades++;
-    upgradeShopPoints.innerHTML = Game.upgrades;
-  }
+  decreaseStat('max speed', shopCurrMaxSpeed, false, 2, 1);
 });
 
 // rotation speed --------------------------------------------------------------
@@ -150,27 +97,11 @@ let btnDecreaseRotationSpeed = document.querySelector('#btnDecreaseRotationSpeed
 let shopCurrRotationSpeed = document.querySelector('#c_rotation_speed');
 
 btnIncreaseRotationSpeed.addEventListener('mousedown', function() {
-  if (Game['rotation speed'].default.toFixed(3) >= Game['rotation speed'].max)
-    alert('You have reached max rotation speed limit');
-  else if (Game.upgrades == 0)
-    alert('You have no upgrade points left');
-  else {
-    Game['rotation speed'].default += Game['rotation speed'].factor;
-    shopCurrRotationSpeed.innerHTML = Game['rotation speed'].default.toFixed(3);
-    Game.upgrades--;
-    upgradeShopPoints.innerHTML = Game.upgrades;
-  }
+  increaseStat('rotation speed', shopCurrRotationSpeed, false, 3, 1);
 });
 
 btnDecreaseRotationSpeed.addEventListener('mousedown', function() {
-  if (Game['rotation speed'].default.toFixed(3) <= Game['rotation speed'].min)
-    alert('You have reached min rotation speed limit');
-  else {
-    Game['rotation speed'].default -= Game['rotation speed'].factor;
-    shopCurrRotationSpeed.innerHTML = Game['rotation speed'].default.toFixed(3);
-    Game.upgrades++;
-    upgradeShopPoints.innerHTML = Game.upgrades;
-  }
+  decreaseStat('rotation speed', shopCurrRotationSpeed, false, 3, 1);
 });
 
 // acceleration ----------------------------------------------------------------
@@ -179,27 +110,11 @@ let btnDecreaseAcceleration = document.querySelector('#btnDecreaseAcceleration')
 let shopCurrAcceleration = document.querySelector('#c_acceleration');
 
 btnIncreaseAcceleration.addEventListener('mousedown', function() {
-  if (Game['acceleration'].default.toFixed(3) >= Game['acceleration'].max)
-    alert('You have reached max acceleration limit');
-  else if (Game.upgrades == 0)
-    alert('You have no upgrade points left');
-  else {
-    Game['acceleration'].default += Game['acceleration'].factor;
-    shopCurrAcceleration.innerHTML = Game['acceleration'].default.toFixed(3);
-    Game.upgrades--;
-    upgradeShopPoints.innerHTML = Game.upgrades;
-  }
+  increaseStat('acceleration', shopCurrAcceleration, false, 3, 1);
 });
 
 btnDecreaseAcceleration.addEventListener('mousedown', function() {
-  if (Game['acceleration'].default.toFixed(3) <= Game['acceleration'].min)
-    alert('You have reached min acceleration limit');
-  else {
-    Game['acceleration'].default -= Game['acceleration'].factor;
-    shopCurrAcceleration.innerHTML = Game['acceleration'].default.toFixed(3);
-    Game.upgrades++;
-    upgradeShopPoints.innerHTML = Game.upgrades;
-  }
+  decreaseStat('acceleration', shopCurrAcceleration, false, 3, 1);
 });
 
 // mass ------------------------------------------------------------------------
@@ -208,27 +123,11 @@ let btnDecreaseMass = document.querySelector('#btnDecreaseMass');
 let shopCurrMass = document.querySelector('#c_mass');
 
 btnIncreaseMass.addEventListener('mousedown', function() {
-  if (Game['mass'].default.toFixed(2) >= Game['mass'].max)
-    alert('You have reached max mass limit');
-  else if (Game.upgrades == 0)
-    alert('You have no upgrade points left');
-  else {
-    Game['mass'].default += Game['mass'].factor;
-    shopCurrMass.innerHTML = Game['mass'].default.toFixed(2);
-    Game.upgrades--;
-    upgradeShopPoints.innerHTML = Game.upgrades;
-  }
+  increaseStat('mass', shopCurrMass, true, 2, 1);
 });
 
 btnDecreaseMass.addEventListener('mousedown', function() {
-  if (Game['mass'].default.toFixed(2) <= Game['mass'].min)
-    alert('You have reached min mass limit');
-  else {
-    Game['mass'].default -= Game['mass'].factor;
-    shopCurrMass.innerHTML = Game['mass'].default.toFixed(2);
-    Game.upgrades++;
-    upgradeShopPoints.innerHTML = Game.upgrades;
-  }
+  decreaseStat('mass', shopCurrMass, true, 2, 1);
 });
 
 // time ------------------------------------------------------------------------
@@ -237,27 +136,11 @@ let btnDecreaseTime = document.querySelector('#btnDecreaseTime');
 let shopCurrTime = document.querySelector('#c_time');
 
 btnIncreaseTime.addEventListener('mousedown', function() {
-  if (Game['time'].default >= Game['time'].max)
-    alert('You have reached max time limit');
-  else if (Game.upgrades == 0)
-    alert('You have no upgrade points left');
-  else {
-    Game['time'].default += Game['time'].factor;
-    shopCurrTime.innerHTML = Game['time'].default;
-    Game.upgrades--;
-    upgradeShopPoints.innerHTML = Game.upgrades;
-  }
+  increaseStat('time', shopCurrTime, false, 0, 1);
 });
 
 btnDecreaseTime.addEventListener('mousedown', function() {
-  if (Game['time'].default <= Game['time'].min)
-    alert('You have reached min time limit');
-  else {
-    Game['time'].default -= Game['time'].factor;
-    shopCurrTime.innerHTML = Game['time'].default;
-    Game.upgrades++;
-    upgradeShopPoints.innerHTML = Game.upgrades;
-  }
+  decreaseStat('time', shopCurrTime, false, 0, 1);
 });
 
 // target spawn interval -------------------------------------------------------
@@ -266,27 +149,11 @@ let btnDecreaseTSI = document.querySelector('#btnDecreaseTSI');
 let shopCurrTSI = document.querySelector('#c_target_spawn_interval');
 
 btnIncreaseTSI.addEventListener('mousedown', function() {
-  if (Game['target spawn interval'].default >= Game['target spawn interval'].max)
-    alert('You have reached max target spawn interval limit');
-  else if (Game.upgrades == 0)
-    alert('You have no upgrade points left');
-  else {
-    Game['target spawn interval'].default += Game['target spawn interval'].factor;
-    shopCurrTSI.innerHTML = Game['target spawn interval'].default;
-    Game.upgrades--;
-    upgradeShopPoints.innerHTML = Game.upgrades;
-  }
+  increaseStat('target spawn interval', shopCurrTSI, true, 0, 1);
 });
 
 btnDecreaseTSI.addEventListener('mousedown', function() {
-  if (Game['target spawn interval'].default <= Game['target spawn interval'].min)
-    alert('You have reached min target spawn interval limit');
-  else {
-    Game['target spawn interval'].default -= Game['target spawn interval'].factor;
-    shopCurrTSI.innerHTML = Game['target spawn interval'].default;
-    Game.upgrades++;
-    upgradeShopPoints.innerHTML = Game.upgrades;
-  }
+  decreaseStat('target spawn interval', shopCurrTSI, true, 0, 1);
 });
 
 // drop chance -----------------------------------------------------------------
@@ -295,25 +162,9 @@ let btnDecreaseDropChance = document.querySelector('#btnDecreaseDropChance');
 let shopCurrDropChance = document.querySelector('#c_drop_chance');
 
 btnIncreaseDropChance.addEventListener('mousedown', function() {
-  if (Game['drop chance'].default.toFixed(3) >= Game['drop chance'].max)
-    alert('You have reached max drop chance limit');
-  else if (Game.upgrades == 0)
-    alert('You have no upgrade points left');
-  else {
-    Game['drop chance'].default += Game['drop chance'].factor;
-    shopCurrDropChance.innerHTML = Game['drop chance'].default.toFixed(3);
-    Game.upgrades--;
-    upgradeShopPoints.innerHTML = Game.upgrades;
-  }
+  increaseStat('drop chance', shopCurrDropChance, false, 3, 1);
 });
 
 btnDecreaseDropChance.addEventListener('mousedown', function() {
-  if (Game['drop chance'].default.toFixed(3) <= Game['drop chance'].min)
-    alert('You have reached min drop chance limit');
-  else {
-    Game['drop chance'].default -= Game['drop chance'].factor;
-    shopCurrDropChance.innerHTML = Game['drop chance'].default.toFixed(3);
-    Game.upgrades++;
-    upgradeShopPoints.innerHTML = Game.upgrades;
-  }
+  decreaseStat('drop chance', shopCurrDropChance, false, 3, 1);
 });
